7a3b13e5da54d28cff2eece873598e2e5b10d934
LindaPermata <permatalinda531@gmail.com>
2025-08-08 10:21:09 +0700

    todos-list

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const port = 3001;
+const port = process.env.PORT || 3001;
+const { router: todoRoutes, todos } = require("./routes/todo.js");
+
+app.use(cors());
 app.use(express.json());
 app.set("view engine", "ejs");
 
-const todoRoutes = require("./routes/todo.js");
-const { todos } = require("./routes/todo.js");
-
-app.use(express.json());
 app.use("/todos", todoRoutes);
 
+app.get("/todos-data", (req, res) => {
+  res.json(todos);
+});
+
+app.get("/todos-list", (req, res) => {
+  res.render("todos-page", { todos: todos });
+});
+
 app.get("/", (req, res) => {
-  res.render( "index" );
+  res.render("index");
 });
 
 app.get("/contact", (req, res) => {
@@ -20,9 +28,9 @@ app.get("/contact", (req, res) => {
 });
 
 app.use((req, res) => {
-  res.status(404).send("Page not found");
+  res.status(404).send("404 - Page Not Found");
 });
 
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+  console.log(`Srver running on http://localhost:${port}`)
 });
