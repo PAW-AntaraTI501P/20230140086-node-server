6f40b467f234204243734e3f8fdd19d2f973f4db
LindaPermata <permatalinda531@gmail.com>
2025-08-12 10:09:20 +0700

    connect database dan view data

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const todoRoutes = require("./routes/todo/db");
 const cors = require("cors");
+const db = require("./database/db");
 const port = process.env.PORT || 3001;
-const { router: todoRoutes, todos } = require("./routes/todo.js");
+const { todos } = require("./routes/todo.js");
+
 
 app.use(cors());
 app.use(express.json());
@@ -25,6 +28,15 @@ app.get("/", (req, res) => {
 
 app.get("/contact", (req, res) => {
   res.render("contact");
+});
+
+app.get("/todo-view", (req, res) => {
+  db.query("SELECT * FROM todos", (err, todos) => {
+    if(err) {return res.status(500).send("Internal Server Error");}
+    res.render("todo", {
+      todos: todos,
+    });
+  });
 });
 
 app.use((req, res) => {
