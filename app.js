e26120e509c4c5e0951d13ced3c2b341b53c6705
LindaPermata <permatalinda531@gmail.com>
2025-08-15 10:35:33 +0700

    template views

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const todoRoutes = require("./routes/todo/db");
+const { router: todoRoutes, todos} = require("./routes/todo");
 const cors = require("cors");
 const db = require("./database/db");
 const port = process.env.PORT || 3001;
-const { todos } = require("./routes/todo.js");
 
 
+const expressLayouts = require("express-ejs-layouts");
+app.use(expressLayouts);
 app.use(cors());
 app.use(express.json());
 app.set("view engine", "ejs");
@@ -19,30 +20,35 @@ app.get("/todos-data", (req, res) => {
 });
 
 app.get("/todos-list", (req, res) => {
-  res.render("todos-page", { todos: todos });
+  res.render("todos-page", { todos: todos, layout: "layouts/main-layout" });
 });
 
+
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", {
+    layout: "layouts/main-layout",
+  });
 });
 
 app.get("/contact", (req, res) => {
-  res.render("contact");
+  res.render("contact", {layout: "layouts/main-layout"});
 });
 
 app.get("/todo-view", (req, res) => {
   db.query("SELECT * FROM todos", (err, todos) => {
-    if(err) {return res.status(500).send("Internal Server Error");}
-    res.render("todo", {
-      todos: todos,
-    });
+    if (err) {
+      console.error("Error fetching todos:", err);
+      return res.status(500).send("Internal Server Error");
+    }
+    res.render("todo", { todos: todos, layout: "layouts/main-layout" });
   });
 });
+
 
 app.use((req, res) => {
   res.status(404).send("404 - Page Not Found");
 });
 
 app.listen(port, () => {
-  console.log(`Srver running on http://localhost:${port}`)
+  console.log(`Server running on http://localhost:${port}`)
 });
